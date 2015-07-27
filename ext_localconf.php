2c6c7a1a5ec68cf2da45335aba772337fb31a497
Ingo Renner <ingo@typo3.org>
2015-07-26 22:27:50 -0700

    Remove ext_autoload.php
    Not needed anymore after changing to namespaces

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,6 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-   # ----- # ----- # ----- # ----- # ----- # ----- # ----- # ----- # ----- #
-
-	// trigger loading of ext_autoload.php
-//\ApacheSolrForTypo3\Solrgrouping\ClassLoader::loadClasses();
-
 \Tx_Solr_Search_SearchComponentManager::registerSearchComponent(
 	'grouping',
 	'ApacheSolrForTypo3\Solrgrouping\Search\GroupingComponent'
