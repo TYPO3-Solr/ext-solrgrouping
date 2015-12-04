b6eae3ed036c989bc12b781bb73c6f0b9d6b5fea
Timo Schmidt <timo.schmidt@dkd.de>
2015-12-04 12:28:04 +0100

    * Set version to 1.1.0 * Support TYPO3 Version 7.6 * Added documentation with examples from forge

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Apache Solr for TYPO3 - Result Grouping',
 	'description' => 'Result Grouping',
-	'version' => '1.1.0-dev',
+	'version' => '1.1.0',
 	'category' => 'plugin',
 	'author' => 'Ingo Renner',
 	'author_email' => 'ingo@typo3.org',
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'solr' => '3.1.0',
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '6.2.0-0.0.0',
+			'typo3' => '6.2.0-7.6.99',
 		),
 		'conflicts' => array(
 		),
